0d37c2d1a696a598a1fe8403665b361c0dd74d0c
Ashraful <ashraful.abh@gmail.com>
2026-05-09 07:07:04 +0530

    Updated globals.css and layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Abhayapuri Care Hospital | Best Healthcare in Bongaigaon",
@@ -41,7 +30,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      className="h-full antialiased scroll-smooth"
     >
       <head>
         {/* ✅ Font Awesome CDN Link for Icons */}
